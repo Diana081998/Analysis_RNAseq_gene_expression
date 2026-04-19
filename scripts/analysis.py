2cdf84942b70ea2f098781010ca70a6e351551db
Diana Campoverde <dianacampo1998@hotmail.com>
2026-04-19 16:55:33 -0500

    Add analysis script

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,1 +1,9 @@
-print("RNA-seq analysis started")
+# Analysis RNA_seq Gene Expression
+# Dataset: HIV RNA_seq (GSE57763)
+
+import pandas as pd
+
+# Load dataset
+file_path = "../data/GSE57763_series_matrix.txt"
+
+print ("RNA_seq analysis script loaded successfully")
